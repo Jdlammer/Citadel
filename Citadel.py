58f8f9bc9124f61f440a73ee227db4d5eb146978
Justice Lammers <justicelammers11@gmail.com>
2018-06-19 12:56:45 -0700

    Character class started

---
+++ Citadel.py
@@ -0,0 +1,42 @@
+class Player:
+    def __init__(self,name=None,attack=None,defense=None):
+        self.name = name
+        self.attack = 5
+        self.defense = 10
+        self.inventory = {
+            "Backpack" : [],
+            "Map" : []}
+        self.coordinates = {
+            x = 0
+            y = 0
+        }
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, x):
+        self.name = x
+
+    def get_attack(self):
+        return self.attack
+
+    def set_attack(self, x):
+        self.attack = x
+
+    def get_defense(self):
+        return self.defense
+
+    def set_defense(self, x):
+        self.defense = x
+
+    def get_inventory(self):
+        return self.inventory
+
+    def welcome():
+        print("Hello! Welcome to Citadel.")
+
+
+
+
+welcome()
+
